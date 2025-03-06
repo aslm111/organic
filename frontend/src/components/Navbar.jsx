bff9a4a242c97f1d2072fad8701ead929640bf0c
preykriti <prakriteeshrestha2@gmail.com>
2025-03-06 15:20:21 +0545

    colors

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="bg-rose-800 text-3xl font-bold">Navbar</nav>;
+  return <nav className="bg-rose-800 text-3xl font-bold text">Navbar</nav>;
 };
 
 export default Navbar;
